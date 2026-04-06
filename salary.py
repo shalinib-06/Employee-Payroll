ea7c4437d7442e98a2a172c8c0ac2b0a2eaec4b2
Shalini B <shalinibalaji61@gmail.com>
2026-04-06 10:13:03 +0530

    added code file

---
+++ salary.py
@@ -0,0 +1,10 @@
+def calculate_salary(basic_pay):
+    hra = 0.2 * basic_pay
+    da = 0.1 * basic_pay
+    total_salary = basic_pay + hra + da
+    tax = 0.05 * total_salary
+    net_salary = total_salary - tax
+    return net_salary
+
+basic_pay = float(input("Enter amount: "))
+print(f"Net salary: ₹{calculate_salary(basic_pay)}")
